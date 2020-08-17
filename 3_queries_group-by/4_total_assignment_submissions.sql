d9af0d6c7b4023f0d76381283a0936a1bfb35771
Dexyod <dexyod43@gmail.com>
2020-08-17 18:58:24 +0000

    Group by queries completed

---
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -0,0 +1,4 @@
+SELECT cohorts.name as cohort, count(*) as total_submissions
+FROM cohorts JOIN students ON cohort_id = cohorts.id JOIN assignment_submissions ON student_id = students.id
+GROUP BY cohort
+ORDER BY total_submissions desc;
