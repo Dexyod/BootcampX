a246f28d2c6d8a85f75e45846ded13e6c9ef9af5
Dexyod <dexyod43@gmail.com>
2020-08-19 22:17:07 +0000

    connected postgresql db from js app

---
+++ bootcamp_app/students.js
@@ -0,0 +1,38 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool
+  .query(
+    `
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`
+  )
+  .then((res) => {
+    console.table(res.rows);
+  })
+  .catch((err) => console.error("query error", err.stack));
+
+const [, , date, limit] = process.argv;
+pool
+  .query(
+    `SELECT students.id as id, students.name as student, cohorts.name as cohort
+    FROM students JOIN cohorts ON cohort_id = cohorts.id
+    WHERE cohorts.name LIKE '${date}%'
+    LIMIT ${limit || 5};`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
